1c790c99bb0a843a6e65e4c2b5d6509dc73fa422
Krishna W <ramain77@gmail.com>
2020-11-22 10:13:56 +0700

    add 3 navigations

---
+++ src/components/GuestNav.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav id="nav" class="flex justify-between items-center px-16 py-8">
+    <img src="./../assets/logo.svg" alt="larns logo" class="w-24" />
+
+    <div class="-mr-6">
+      <router-link
+        :to="{ name: 'Home' }"
+        class="inline-block text-black-300 font-semibold px-6"
+      >
+        <span class="inline-block pb-1">Home</span>
+      </router-link>
+      <router-link
+        :to="{ name: 'Signin' }"
+        class="inline-block text-black-300 font-semibold px-6"
+      >
+        <span class="inline-block pb-1">Sign In</span>
+      </router-link>
+      <router-link
+        :to="{ name: 'Signup' }"
+        class="inline-block text-black-300 font-semibold px-6"
+      >
+        <span class="inline-block pb-1">Sign Up</span>
+      </router-link>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "GuestNav",
+};
+</script>
+
+<style scoped>
+#nav a.router-link-exact-active span {
+  @apply border-b-2 border-red-500;
+}
+</style>
