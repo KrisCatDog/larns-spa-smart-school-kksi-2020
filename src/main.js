a219baf98f90623aeef3d6e82d6a778257091c20
Krishna W <ramain77@gmail.com>
2020-11-23 12:57:42 +0700

    upgrade tailwind version & install axios

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./assets/tailwind.css";
+import VueAxios from "vue-axios";
+import axios from "axios";
+
+axios.defaults.baseURL = process.env.VUE_APP_API_URL;
 
 createApp(App)
   .use(store)
   .use(router)
+  .use(VueAxios, axios)
   .mount("#app");
