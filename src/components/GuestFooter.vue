06a5a8a5f2e6361f3618c7046342ad0f35d82ec5
Krishna W <ramain77@gmail.com>
2020-11-24 11:55:35 +0700

    add footer comp. for guest

---
+++ src/components/GuestFooter.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer class="px-8 sm:px-16 py-4 sm:py-8">
+    <p class="text-xs sm:text-sm 2xl:text-base">
+      &copy; Copyright 2020 [Krishna W, M. Genta, Irham J]. All right reserved.
+    </p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "GuestFooter",
+};
+</script>
