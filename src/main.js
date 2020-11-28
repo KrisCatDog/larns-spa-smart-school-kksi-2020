5949629816f96bf47a7d385ffdad1bdde2d4d559
Krishna W <ramain77@gmail.com>
2020-11-28 17:43:42 +0700

    remove unnesessary code

--- src/main.js
+++ src/main.js
@@ -3,13 +3,12 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./assets/tailwind.css";
-import VueAxios from "vue-axios";
 import axios from "axios";
 
 axios.defaults.baseURL = process.env.VUE_APP_API_URL;
+axios.defaults.withCredentials = true;
 
 createApp(App)
   .use(store)
   .use(router)
-  .use(VueAxios, axios)
   .mount("#app");
