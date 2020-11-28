38b310e6650549dd10886e4f12a13bd8de4f618b
Krishna W <ramain77@gmail.com>
2020-11-28 22:05:22 +0700

    add assignments module api (load & store)

---
+++ src/modules/assignments.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+import { reactive, toRefs } from "vue";
+
+const state = reactive({
+  assignments: null,
+});
+
+export default function useAssignments() {
+  const load = async (id) => {
+    try {
+      const response = await axios.get(`/classrooms/${id}/assignments`, {
+        headers: {
+          Accept: "application/json",
+          Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+        },
+      });
+
+      state.assignments = response.data.data;
+    } catch (e) {
+      console.log(e.response);
+    }
+  };
+
+  const store = async (classroomId, data) => {
+    try {
+      const response = await axios.post(
+        `/classrooms/${classroomId}/assignments`,
+        data,
+        {
+          headers: {
+            Accept: "application/json",
+            Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+          },
+        }
+      );
+
+      state.assignments.push(response.data.data);
+    } catch (e) {
+      console.log(e.response);
+    }
+  };
+
+  return { ...toRefs(state), load, store };
+}
