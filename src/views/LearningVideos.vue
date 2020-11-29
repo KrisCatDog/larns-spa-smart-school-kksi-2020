381d5b923dfdb4ce55b27da4b985e678948f602a
Krishna W <ramain77@gmail.com>
2020-11-29 19:31:42 +0700

    add submit video

--- src/views/LearningVideos.vue
+++ src/views/LearningVideos.vue
@@ -9,7 +9,7 @@
 
     <template #fallback>
       <div
-        class="h-14 px-16 py-3 border bg-white shadow-sm rounded-md w-full mx-auto"
+        class="h-14 px-16 py-4 border bg-white shadow-sm rounded-md w-full mx-auto"
       >
         <div class="animate-pulse">
           <div class="h-4 bg-red-400 rounded w-5/12"></div>
@@ -22,41 +22,52 @@
     <section
       class="w-full md:max-w-6xl grid grid-cols-1 md:grid-cols-8 gap-4 sm:gap-8 px-4 sm:px-16 md:px-0 mb-16"
     >
-      <div
-        class="h-28 col-span-6 md:col-span-2 flex flex-col justify-around bg-white p-6 rounded-md shadow-sm"
-      >
-        <p class="text-xs uppercase text-red-500 font-bold">My Profile</p>
+      <Suspense>
+        <template #default>
+          <SideClassroomProfile />
+        </template>
 
-        <div class="flex items-center mt-3">
-          <div class="w-10 h-10 bg-gray-200 rounded-full">
-            <img
-              src="https://placekitten.com/200/202"
-              alt=""
-              class="w-full h-full rounded-full"
-            />
+        <template #fallback>
+          <div
+            class="h-28 col-span-6 md:col-span-2 border bg-white shadow-sm rounded-md p-6 w-full mx-auto"
+          >
+            <div class="animate-pulse">
+              <div class="h-3 bg-red-400 rounded w-1/2"></div>
+              <div class="flex space-x-4 mt-3">
+                <div class="rounded-full bg-red-400 h-10 w-10"></div>
+                <div class="flex-1 space-y-3 py-1">
+                  <div class="h-3 bg-red-400 rounded w-3/4"></div>
+                  <div class="space-y-2">
+                    <div class="h-3 bg-red-400 rounded w-5/12"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-
-          <div class="ml-3">
-            <h3 class="font-semibold">CatDog</h3>
-            <p class="text-sm">Teacher</p>
-          </div>
-        </div>
-      </div>
+        </template>
+      </Suspense>
 
       <div class="col-span-6">
-        <div class="bg-white rounded-md shadow-sm px-8 py-5 mb-5 flex flex-col">
+        <form
+          @submit.prevent="submitStoreVideo"
+          class="bg-white rounded-md shadow-sm px-8 py-5 mb-5 flex flex-col"
+          enctype="multipart/form-data"
+        >
           <input
+            v-model="title"
             class="appearance-none bg-gray-100 mb-4 px-4 py-2 font-semibold rounded-md focus:ring focus:ring-red-400 focus:ring-opacity-50 focus:outline-none"
             type="text"
             placeholder="Video title.."
           />
 
           <textarea
+            v-model="description"
             class="appearance-none h-28 bg-gray-100 text-sm font-medium mb-4 px-4 py-2 rounded-md resize-y focus:ring focus:ring-red-400 focus:ring-opacity-50 focus:outline-none"
             placeholder="Video description.."
           ></textarea>
 
           <div class="flex items-center justify-between">
+            <input type="file" @change="selectFile" />
             <button
               class="flex items-center border bg-indigo-500 border-indigo-500 text-white px-4 py-1 mr-3 rounded-full transition duration-100 ease-in-out transform hover:bg-indigo-600 hover:border-indigo-600 focus:translate-y-1 hover:text-white focus:outline-none"
             >
@@ -74,6 +85,7 @@
 
             <div class="flex items-center">
               <button
+                type="submit"
                 class="flex items-center border bg-red-500 border-red-500 text-white px-4 py-1 mr-3 rounded-full transition duration-100 ease-in-out transform hover:bg-red-600 hover:border-red-600 focus:translate-y-1 hover:text-white focus:outline-none"
               >
                 <svg
@@ -107,7 +119,7 @@
               </button>
             </div>
           </div>
-        </div>
+        </form>
 
         <Suspense>
           <template #default>
@@ -138,8 +150,9 @@
 </template>
 
 <script>
-import { defineAsyncComponent } from "vue";
+import { defineAsyncComponent, ref } from "vue";
 import ClassroomNav from "./../components/ClassroomNav";
+import axios from "axios";
 
 const ClassroomHeader = defineAsyncComponent(() =>
   import("./../components/ClassroomHeader")
@@ -149,11 +162,47 @@ const LearningVideosList = defineAsyncComponent(() =>
   import("./../components/LearningVideosList")
 );
 
+const SideClassroomProfile = defineAsyncComponent(() =>
+  import("./../components/SideClassroomProfile")
+);
+
 export default {
   components: {
     ClassroomNav,
     ClassroomHeader,
     LearningVideosList,
+    SideClassroomProfile,
+  },
+  setup() {
+    const attachmentVideo = ref(null);
+    const title = ref(null);
+    const description = ref(null);
+
+    const data = new FormData();
+
+    function selectFile(event) {
+      attachmentVideo.value = event.target.files[0];
+    }
+
+    async function submitStoreVideo() {
+      data.append("attachment_video", attachmentVideo.value);
+      data.append("title", title.value);
+      data.append("description", description.value);
+
+      await axios.post("/classrooms/1/class-videos", data, {
+        headers: {
+          Accept: "application/json",
+          Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+        },
+      });
+    }
+
+    return {
+      selectFile,
+      submitStoreVideo,
+      title,
+      description,
+    };
   },
 };
 </script>
