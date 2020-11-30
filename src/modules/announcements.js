aa5f167d84d8c8848dcd1885d2d2444b833052bf
Krishna W <ramain77@gmail.com>
2020-11-30 22:11:44 +0700

    add try catch for load & store

--- src/modules/announcements.js
+++ src/modules/announcements.js
@@ -5,29 +5,37 @@ const state = reactive({ announcements: [] });
 
 export default function useAnnouncements() {
   const load = async (id) => {
-    const response = await axios.get(`/classrooms/${id}/announcements`, {
-      headers: {
-        Accept: "application/json",
-        Authorization: `Bearer ${localStorage.getItem("authToken")}`,
-      },
-    });
-
-    state.announcements = response.data.data;
-  };
-
-  const store = async (classroomId, data) => {
-    const response = await axios.post(
-      `/classrooms/${classroomId}/announcements`,
-      data,
-      {
+    try {
+      const response = await axios.get(`/classrooms/${id}/announcements`, {
         headers: {
           Accept: "application/json",
           Authorization: `Bearer ${localStorage.getItem("authToken")}`,
         },
-      }
-    );
+      });
 
-    state.announcements.unshift(response.data.data);
+      state.announcements = response.data.data;
+    } catch (e) {
+      console.log(e.response);
+    }
+  };
+
+  const store = async (classroomId, data) => {
+    try {
+      const response = await axios.post(
+        `/classrooms/${classroomId}/announcements`,
+        data,
+        {
+          headers: {
+            Accept: "application/json",
+            Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+          },
+        }
+      );
+
+      state.announcements.unshift(response.data.data);
+    } catch (e) {
+      console.log(e.response);
+    }
   };
 
   return { ...toRefs(state), load, store };
