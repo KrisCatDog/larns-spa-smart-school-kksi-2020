c660a9b5b5d9f42c811950f1eaed27d414ba3f46
Krishna W <ramain77@gmail.com>
2020-12-03 08:13:39 +0700

    add discussions view

---
+++ src/views/Discussions.vue
@@ -0,0 +1,155 @@
+<template>
+  <ClassroomNav />
+
+  <Suspense>
+    <template #default>
+      <ClassroomHeader
+        svgPath="M10 5h-5.033c-2.743 0-4.967 2.239-4.967 5 0 2.762 2.224 5 4.967 5h5.033v-10zm2.007 17.475c-.52-.424-.902-.994-1.095-1.637l-1.151-3.827h-5.125l1.905 5.883c.214.659.828 1.106 1.521 1.106h3.439c.358 0 .677-.225.797-.562.12-.337.015-.713-.263-.939l-.028-.024zm-.007-17.475v2.014c3.088-.445 6.379-1.603 10-3.544v13.064c-3.607-1.924-6.958-3.109-10-3.548v2.014c3.164.509 7.16 1.992 12 5v-20c-4.964 3.085-8.877 4.502-12 5z"
+      />
+    </template>
+
+    <template #fallback>
+      <div
+        class="h-14 px-16 py-4 border bg-white shadow-sm rounded-md w-full mx-auto"
+      >
+        <div class="animate-pulse">
+          <div class="h-4 bg-red-400 rounded w-5/12"></div>
+        </div>
+      </div>
+    </template>
+  </Suspense>
+
+  <div class="flex justify-center mt-5">
+    <section
+      class="w-full md:max-w-6xl grid grid-cols-1 md:grid-cols-8 gap-4 sm:gap-8 px-4 sm:px-16 md:px-0 mb-16"
+    >
+      <Suspense>
+        <template #default>
+          <SideClassroomProfile />
+        </template>
+
+        <template #fallback>
+          <div
+            class="h-28 col-span-6 md:col-span-2 border bg-white shadow-sm rounded-md p-6 w-full mx-auto"
+          >
+            <div class="animate-pulse">
+              <div class="h-3 bg-red-400 rounded w-1/2"></div>
+              <div class="flex space-x-4 mt-3">
+                <div class="rounded-full bg-red-400 h-10 w-10"></div>
+                <div class="flex-1 space-y-3 py-1">
+                  <div class="h-3 bg-red-400 rounded w-3/4"></div>
+                  <div class="space-y-2">
+                    <div class="h-3 bg-red-400 rounded w-5/12"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </template>
+      </Suspense>
+
+      <div class="col-span-6">
+        <div class="bg-white rounded-md shadow-sm px-8 py-5 mb-5 flex flex-col">
+          <input
+            class="appearance-none bg-gray-100 mb-4 px-4 py-2 font-semibold rounded-md focus:ring focus:ring-red-400 focus:ring-opacity-50 focus:outline-none"
+            type="text"
+            placeholder="Exam title.."
+          />
+
+          <textarea
+            class="appearance-none h-28 bg-gray-100 text-sm font-medium mb-4 px-4 py-2 rounded-md resize-y focus:ring focus:ring-red-400 focus:ring-opacity-50 focus:outline-none"
+            placeholder="Exam description.."
+          ></textarea>
+
+          <div class="flex items-center self-end">
+            <button
+              class="flex items-center border bg-red-500 border-red-500 text-white px-4 py-1 mr-3 rounded-full transition duration-100 ease-in-out transform hover:bg-red-600 hover:border-red-600 focus:translate-y-1 hover:text-white focus:outline-none"
+            >
+              <svg
+                class="fill-current w-4 h-4"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  d="M18.363 8.464l1.433 1.431-12.67 12.669-7.125 1.436 1.439-7.127 12.665-12.668 1.431 1.431-12.255 12.224-.726 3.584 3.584-.723 12.224-12.257zm-.056-8.464l-2.815 2.817 5.691 5.692 2.817-2.821-5.693-5.688zm-12.318 18.718l11.313-11.316-.705-.707-11.313 11.314.705.709z"
+                />
+              </svg>
+              <span class="inline-block ml-2">Post!</span>
+            </button>
+
+            <button
+              class="flex items-center border border-gray-800 px-4 py-1 rounded-full transition duration-100 ease-in-out hover:bg-yellow-400 hover:border-yellow-400 hover:text-white focus:outline-none"
+            >
+              <svg
+                class="fill-current w-4 h-4 stroke-current stroke-1"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+              >
+                <path
+                  d="M12 11.293l10.293-10.293.707.707-10.293 10.293 10.293 10.293-.707.707-10.293-10.293-10.293 10.293-.707-.707 10.293-10.293-10.293-10.293.707-.707 10.293 10.293z"
+                />
+              </svg>
+
+              <span class="inline-block ml-2">Cancel</span>
+            </button>
+          </div>
+        </div>
+
+        <Suspense>
+          <template #default>
+            <ExamsList />
+          </template>
+
+          <template #fallback>
+            <div
+              class="border bg-white shadow-sm rounded-md py-5 px-8 w-full mx-auto"
+            >
+              <div class="animate-pulse">
+                <div class="flex space-x-4">
+                  <div class="rounded-full bg-red-400 h-10 w-10"></div>
+                  <div class="flex-1 space-y-3 py-1">
+                    <div class="h-4 bg-red-400 rounded w-4/6"></div>
+                    <div class="space-y-2">
+                      <div class="h-4 bg-red-400 rounded w-5/12"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </template>
+        </Suspense>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import { defineAsyncComponent } from "vue";
+import ClassroomNav from "./../components/ClassroomNav";
+
+const ClassroomHeader = defineAsyncComponent(() =>
+  import("./../components/ClassroomHeader")
+);
+
+const ExamsList = defineAsyncComponent(() =>
+  import("./../components/ExamsList")
+);
+
+const SideClassroomProfile = defineAsyncComponent(() =>
+  import("./../components/SideClassroomProfile")
+);
+
+export default {
+  components: {
+    ClassroomNav,
+    ClassroomHeader,
+    ExamsList,
+    SideClassroomProfile,
+  },
+};
+</script>
+
+<style>
+</style>
