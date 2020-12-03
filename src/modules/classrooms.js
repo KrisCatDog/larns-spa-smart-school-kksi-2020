8f81415e2dc04d9aa9c43b43a322f5ca91bdffcf
Krishna W <ramain77@gmail.com>
2020-12-03 08:12:22 +0700

    add isSettingModalActive to each item

--- src/modules/classrooms.js
+++ src/modules/classrooms.js
@@ -1,29 +1,45 @@
 import axios from "axios";
 import { reactive, toRefs } from "vue";
 
-const state = reactive({ classrooms: [] });
+const state = reactive({ classrooms: [], errorStore: null });
 
 export default function useClassrooms() {
   const load = async () => {
-    const response = await axios.get(`/classrooms`, {
-      headers: {
-        Accept: "application/json",
-        Authorization: `Bearer ${localStorage.getItem("authToken")}`,
-      },
-    });
+    state.classrooms = [];
 
-    state.classrooms = response.data.data;
+    try {
+      const response = await axios.get(`/classrooms`, {
+        headers: {
+          Accept: "application/json",
+          Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+        },
+      });
+
+      response.data.data.forEach((item) => {
+        item.isSettingModalActive = false;
+
+        state.classrooms.push(item);
+      });
+    } catch (e) {
+      console.log(e.response);
+    }
   };
 
   const store = async (data) => {
-    const response = await axios.post(`/classrooms`, data, {
-      headers: {
-        Accept: "application/json",
-        Authorization: `Bearer ${localStorage.getItem("authToken")}`,
-      },
-    });
+    state.errorStore = null;
 
-    state.classrooms.unshift(response.data.data);
+    try {
+      const response = await axios.post(`/classrooms`, data, {
+        headers: {
+          Accept: "application/json",
+          Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+        },
+      });
+
+      state.classrooms.unshift(response.data.data);
+    } catch (e) {
+      state.errorStore = e.response.status;
+    }
   };
 
   const joinClass = async (data) => {
@@ -36,15 +52,15 @@ export default function useClassrooms() {
       });
 
       state.classrooms.unshift(response.data.data);
-    } catch(e) {
+    } catch (e) {
       console.log(e.response);
     }
-  }
+  };
 
   return {
     ...toRefs(state),
     load,
     store,
-    joinClass
+    joinClass,
   };
 }
