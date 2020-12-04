66e00089afa9f7b7db5bbbf8403797156a52621c
Krishna W <ramain77@gmail.com>
2020-12-04 16:39:02 +0700

    add modal create global animation

--- src/App.vue
+++ src/App.vue
@@ -9,4 +9,21 @@
   -moz-osx-font-smoothing: grayscale;
   @apply min-h-screen bg-gray-100 text-gray-700;
 }
+.modal-create-enter-active {
+  animation: bounce-in 0.5s;
+}
+.modal-create-leave-active {
+  animation: bounce-in 0.4s reverse;
+}
+@keyframes bounce-in {
+  0% {
+    transform: translate(-50%, -50%) scale(0);
+  }
+  50% {
+    transform: translate(-50%, -50%) scale(1.25);
+  }
+  100% {
+    transform: translate(-50%, -50%) scale(1);
+  }
+}
 </style>
