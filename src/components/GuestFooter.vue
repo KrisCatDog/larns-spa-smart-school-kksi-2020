0066491e9660b36e03ec8b9aecb3b5bc88249142
Krishna W <ramain77@gmail.com>
2021-01-23 00:08:21 +0700

    change copyright text

--- src/components/GuestFooter.vue
+++ src/components/GuestFooter.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="px-6 sm:px-16 py-4 sm:py-8">
     <p class="text-xs sm:text-sm 2xl:text-base">
-      &copy; Copyright 2020 [Krishna W, M. Genta, Irham J]. All right reserved.
+      &copy; Copyright 2020 Krishna W. All right reserved.
     </p>
   </footer>
 </template>
